d8fa15ddb71f1411e0a8e800f1bdc7815e3303cc
kshravi86 <kshravi86@gmail.com>
2018-08-03 11:44:36 +0530

    hhrhr

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $routeProvider.when('/view1', {templateUrl: 'partials/ctci/1_2_reverse.py.html'});
         $routeProvider.when('/view2', {templateUrl: 'partials/ctci/1_3_duplicate_characters.py.html'});
         $routeProvider.when('/view3', {templateUrl: 'partials/ctci/1_4_anagrams.py.html'});
-        $routeProvider.when('/view4', {templateUrl: 'partials/do_while.py.html'});
+        $routeProvider.when('/view4', {templateUrl: 'partials/ctci/1_5_spaces.py.html'});
         $routeProvider.when('/view5', {templateUrl: 'partials/the_dict.py.html'});
         $routeProvider.when('/view6', {templateUrl: 'partials/the_list.py.html'});
         $routeProvider.when('/view7', {templateUrl: 'partials/the_set.py.html'});
