c61cc3235c29da03edc132c577218889a55c8b99
kshravi86 <kshravi86@gmail.com>
2018-08-05 13:34:37 +0530

    v4g4g

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
 	   $routeProvider.when("/view17",{templateUrl:'partials/ctci/non_repeating_character.py.html'});
 	   $routeProvider.when("/view18",{templateUrl:'partials/ch1/02-is-permutation.py.html'});
 	   $routeProvider.when("/view19",{templateUrl:'partials/ch1/06-string-compression.py.html'});
-	  $routeProvider.when("/view20",{templateUrl:'partials/P12_DecimalToBinary.py.html'});
+	  $routeProvider.when("/view20",{templateUrl:'partials/ch1/07-rotate-matrix.py.html'});
 	  $routeProvider.when("/view21",{templateUrl:'partials/P34_Stack.py.html'});
 	  $routeProvider.when("/view22",{templateUrl:'partials/P53_ShellSort.py.html'});
 	  $routeProvider.when("/view23",{templateUrl:'partials/IQtest.html'});
